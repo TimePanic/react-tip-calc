1a836a212356482f50dfc816c2011430f1c3b07c
Dan Lyndon <daniellyndon21@gmail.com>
2020-06-30 18:28:15 +0100

    Changed to style.scss

--- src/App.js
+++ src/App.js
@@ -27,20 +27,21 @@ const App = () => {
   
     return (
       <div className="App">
+        <h1 id="header">Tip Calculator</h1>
         <div className="tipForm">
           <form onSubmit={calculate} id="form">
             <h4 className="title">Total price:</h4>
             <label id="costLabel">
               £
-              <input type="number" name="totalCost" id="totalCost" min="1" required/>
+              <input type="number" name="totalCost" id="totalCost" min="1" step=".01" required/>
             </label>
             <h4 className="title">Number of people:</h4>
             <label id="peopleLabel">
-              <input type="number" name="amountOfPeople" id="amountOfPeople" min="1"/>
+              <input type="number" name="amountOfPeople" id="amountOfPeople" min="1" required/>
               people
             </label>
             <h4 className="title">% of tip</h4>
-            <select name="amountOfTip" id="amountOfTip">
+            <select name="amountOfTip" id="amountOfTip" required>
               <option value="5">5%</option>
               <option value="10">10%</option>
               <option value="15">15%</option>
@@ -51,9 +52,11 @@ const App = () => {
             <button type="submit" id="submitBtn">Calculate</button>
           </form>
 
-          <h2>{ calculated ? `Your tip is: £${price}` : "" }</h2>
-          <h3>{ calculated ? `£${tip} per person` : "" }</h3>
-          { calculated ? <button onClick={reset}>Reset</button> : "" }
+          <div className="results">
+            <h2>{ calculated ? `Your tip is: £${price}` : "" }</h2>
+            <h3>{ calculated ? `£${tip} per person` : "" }</h3>
+            { calculated ? <button onClick={reset} id="resetBtn">Reset</button> : "" }
+          </div>
         </div>
       </div>
     );
